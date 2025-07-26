8c5da27c675dce943627833c4bd47c17b2e79f41
SeyyidOS <seyyidosmansevgili@gmail.com>
2025-07-26 12:41:52 +0300

    Visual improvements

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/Navbar.css";
 
 const Navbar: React.FC = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
+
   return (
     <div className="navbar-wrapper">
       <nav className="navbar">
         <div className="navbar-brand">⚽ Fifa Manager</div>
-        <div className="navbar-links">
-          <NavLink to="/" end className="nav-link">
+        <div className={`navbar-links ${showMenu ? "show" : ""}`}>
+          <NavLink
+            to="/"
+            end
+            className="nav-link"
+            onClick={() => setShowMenu(false)}
+          >
             Home
           </NavLink>
-          <NavLink to="/leaderboard" className="nav-link">
+          <NavLink
+            to="/leaderboard"
+            className="nav-link"
+            onClick={() => setShowMenu(false)}
+          >
             Leaderboard
           </NavLink>
-          <NavLink to="/admin" className="nav-link">
+          <NavLink
+            to="/admin"
+            className="nav-link"
+            onClick={() => setShowMenu(false)}
+          >
             Admin
           </NavLink>
+        </div>
+        <div className="hamburger" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </nav>
     </div>
