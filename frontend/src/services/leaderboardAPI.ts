cd90d860b28117b43befe97997d61df14a9a61e4
SeyyidOS <seyyidosmansevgili@gmail.com>
2025-08-24 20:41:53 +0300

    Moved to render server

--- frontend/src/services/leaderboardAPI.ts
+++ frontend/src/services/leaderboardAPI.ts
@@ -1,6 +1,6 @@
 // const API_URL = "https://simple-235722045899.us-central1.run.app";
 // const API_URL = "http://localhost:8080";
-const API_URL = "http://173.249.40.69:8080"; // Contabo
+const API_URL = "https://fifadatabaseapp.onrender.com"; // Contabo
 
 export const fetchPlayerLeaderboard = async (startTime: string) => {
   const res = await fetch(
