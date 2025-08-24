5457d594d1f62244a6c8ebc248866499e308349f
SeyyidOS <seyyidosmansevgili@gmail.com>
2025-08-24 15:30:31 +0300

    Moved to contabo server

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,6 @@
-const API_URL = "https://simple-235722045899.us-central1.run.app";
+// const API_URL = "https://simple-235722045899.us-central1.run.app";
 // const API_URL = "http://localhost:8080";
+const API_URL = "http://173.249.40.69:8080"; // Contabo
 
 export const fetchPlayersAPI = async () => {
   const res = await fetch(`${API_URL}/players`);
